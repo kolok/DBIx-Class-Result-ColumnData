af2255439683f13a561e9dd09d36a7b0108938fd
Nicolas Oudard <noudard@weborama.com>
2011-02-09 19:10:15 +0100

    new version 0.05

--- lib/DBIx/Class/Result/ColumnData.pm
+++ lib/DBIx/Class/Result/ColumnData.pm
@@ -13,11 +13,11 @@ It defined relationships methods to extract columns data only of relationships
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 
 =head1 SYNOPSIS
