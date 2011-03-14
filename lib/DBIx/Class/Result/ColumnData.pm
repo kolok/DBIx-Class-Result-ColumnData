4a30b702f654b45d23895a1b454e5c5f4ed0ab09
Nicolas Oudard <noudard@weborama.com>
2011-03-14 14:50:17 +0100

    incremente version

--- lib/DBIx/Class/Result/ColumnData.pm
+++ lib/DBIx/Class/Result/ColumnData.pm
@@ -13,11 +13,11 @@ It defined relationships methods to extract columns data only of relationships
 
 =head1 VERSION
 
-Version 0.07
+Version 0.08
 
 =cut
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 
 =head1 SYNOPSIS
